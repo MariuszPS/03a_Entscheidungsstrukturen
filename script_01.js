d1aa9c7aafa2fb9725a6f4d1f70f4cac0890c830
Mariusz PS <k8kpol@gmail.com>
2022-05-09 14:56:01 +0200

    ok

--- script_01.js
+++ script_01.js
@@ -3,9 +3,17 @@
 // Entscheidungsstrukturen | control flow
 
 // Deklaration + Assigment
-const ageJohn = 30;
+const ageJohn = 35;
 const ageMark = 30;
+
+console.log(ageJohn);
+console.log(ageMark);
 
 // Deklaration
 let isJohnOlder, isJohnEqual;
 
+// Test | Logische Aussage
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
